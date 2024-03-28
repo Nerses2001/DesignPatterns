71af3c18d0eaebe5cd0312513e7b516cd46a558f
Nerses2001 <Nerses2001>
2024-03-28 12:47:59 +0400

    Added program

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using DesignPatterns;
+
+Console.WriteLine(">>>> ProxyExample <<<<");
+StartUp.ProxyExample();
+
+Console.WriteLine(">>>> DontUsingProxyExample <<<<");
+StartUp.DontUsingProxyExample();
+
+Console.WriteLine(">>>> StartBuilderExample <<<<");
+StartUp.StartBuilderExample();
+
+Console.WriteLine();
+Console.WriteLine(">>>> StartStrategyExample <<<<");
+
+StartUp.StartStrategyExample();
+Console.WriteLine();
+
+Console.WriteLine(">>>> StartFactoryExample <<<<");
+StartUp.StartFactoryExample();
